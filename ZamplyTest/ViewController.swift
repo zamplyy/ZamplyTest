2e4a0eddc995a4c4eded2783f0c8b4dfacc5b1de
Joar Karlson <joarkarlson@Joars-MacBook-Pro.local>
2017-05-06 22:50:55 +0200

    Initial Commit

---
+++ ZamplyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZamplyTest
+//
+//  Created by Joar Karlson on 2017-05-06.
+//  Copyright © 2017 Joar Karlson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
