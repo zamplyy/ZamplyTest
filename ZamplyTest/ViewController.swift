8d8e78d3aeb4cf97975757060777132a876971c2
zamplyy <jojar_kga@hotmail.com>
2017-05-06 23:51:27 +0200

    hello jojje
    La till hello jojje också

--- ZamplyTest/ViewController.swift
+++ ZamplyTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Say Hello World")
+        print("Say Hello Jojje också")
     }
 
     override func didReceiveMemoryWarning() {
